160b63eb9e218d3b76cc070ceddf8d0ad9aca149
toure <tourekadiy@gmail.com>
2017-10-05 12:17:59 +0200

    test branch

--- symfony_search_engine/src/AppBundle/Controller/DefaultController.php
+++ symfony_search_engine/src/AppBundle/Controller/DefaultController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Form\productType;
 use AppBundle\Entity\product;
 
+
 class DefaultController extends Controller
 {
     /**
@@ -30,41 +31,44 @@ class DefaultController extends Controller
         $form = $this->createForm(productType::class);
         
         $repository = $this->getDoctrine()->getRepository(product::class);
-        //$products = $repository->findAll();
-    //$request->request->get('form');
+
         
         $price = $request->request->get('appbundle_product')['price'];
-        //$name = $request->request->get('appbundle_product')['name'];
-        //$premium = $request->request->get('appbundle_product')['premium'];
-        //$note = $request->request->get('appbundle_product')['note'];
+        $name = $request->request->get('appbundle_product')['name'];
+        
+        $premium = false;
+        if(isset($request->request->get('appbundle_product')['premium'])){
+            $premium = $request->request->get('appbundle_product')['premium'];
+        }
+
+        $note = $request->request->get('appbundle_product')['note'];
         
         
         
         $query = $repository->createQueryBuilder('p');
+        
+        $query->where('p.enabled = 1');
                 
         
         if($price > 0) {
-            $query->where('p.price > :price')
+            $query->andWhere('p.price > :price')
                   ->setParameter('price', $price);
         }
         
-        
-        /*
-        if($name) {
-            $query->where('p.name == :name')
-                  ->setParameter('name', $name);
+        if($premium == true){
+            $query->andWhere('p.premium = 1');
         }
         
-         if($premium) {
-            $query->where('p.premium == :premium')
-                  ->setParameter('premium', $premium);
+        if($name != '') {
+            $query->andWhere('p.name LIKE :name')
+                  ->setParameter('name', '%'.$name.'%');
         }
         
-         if($note) {
-            $query->where('p.note == :note')
+        if($note > 0) {
+            $query->andWhere('p.note > :note')
                   ->setParameter('note', $note);
         }
-        */
+        
         
         $query = $query->orderBy('p.price', 'ASC')
               ->getQuery();
@@ -76,83 +80,7 @@ class DefaultController extends Controller
             'searchForm' => $form->createView()
         ));
         
-       
-        /*
-        $name = $request->request->get('name');
-        
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$name) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$name);
-        }
-        
-        $description = $request->request->get('description');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$description);
-        }
-        
-        $ref = $request->request->get('ref');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$ref);
-        }
-        
-         $ref = $request->request->get('ref');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$ref);
-        }
-        
-        $enabled = $request->request->get('enabled');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$enabled);
-        }
-        
-          $premium = $request->request->get('premium');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$premium);
-        }
-        
-        $note = $request->request->get('note');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$note);
-        }
-        
-        $created = $request->request->get('created');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$created);
-        }
-        
-        $updated = $request->request->get('updated');
-        $repository = $this->getDoctrine()->getRepository(product::class);
-        $query = $repository->createQueryBuilder('p');
-        if (!$description) {
-            throw $this->createNotFoundException(
-                    'No products found for' .$updated);
-        }
-        
-        $products = $query->getResult();
-       */ 
+
     }
      
   
